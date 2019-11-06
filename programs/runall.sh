42f29719c9389259c9407d0edcda7ece2f919d8a
johnniehard <johnnie@synvinkel.org>
2019-11-06 14:33:25 +0100

    larger tiles and remove unused

--- programs/runall.sh
+++ programs/runall.sh
@@ -6,16 +6,12 @@ set -e
 echo "tiler"
 rm -rf out_tiler
 mkdir out_tiler
-./tiler/tiler.exe 1024 1024 $INPUT_DEM ./out_tiler/%n.tif
+./tiler/tiler.exe 2048 2048 $INPUT_DEM ./out_tiler/%n.tif
 
 echo "breach"
 rm -rf out_breach
 mkdir out_breach
 ./tiled_lindsay_breaching/breaching.exe ./out_tiler/layout.layout ./out_breach/%f.tif
-
-# rm -rf out_flood
-# mkdir out_flood
-# mpirun -n 4 ./parallel_priority_flood/parallel_pf.exe many @evict ./out_tiler/layout.layout ./out_flood/%n.tif
 
 echo "flatres"
 rm -rf out_flatres
@@ -28,5 +24,3 @@ echo "accum"
 rm -rf out_accum
 mkdir out_accum
 mpirun -n 4 ./parallel_d8_accum/parallel_d8_accum.exe many @evict ./out_flatres/layout.layout ./out_accum/%n.tif
-
-# gdal_merge.py out_accum/merged.tif out_accum/*.tif
